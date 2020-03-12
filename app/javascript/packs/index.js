9f8d74fa6df856201d8c0ecfb01fbe29469a647f
Rahul Mody <rmody3@gmail.com>
2020-03-12 04:04:17 -0400

    enter redux

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import createStore from '../create_store';
 import App from '../components/app'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App/>,
+    
+    <Provider store={createStore()}>
+      <App />
+    </Provider>,
     document.body.appendChild(document.createElement('div')),
   )
 })
